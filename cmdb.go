bc1d8e6c0f440cc3dff6db57d8b21e4fbb45c7a5
Matthew Davis <matthew@matthewdavis.io>
2024-11-24 10:12:13 -0600

    bump

--- cmdb.go
+++ cmdb.go
@@ -6,17 +6,27 @@ import (
 	"github.com/steebchen/prisma-client-go/runtime/types"
 )
 
+type ChangeType string
+
+var (
+	ChangeTypeSystemChange ChangeType = "system"
+	ChangeTypeUserChange   ChangeType = "user"
+)
+
 type Change struct {
-	ID      string    `json:"id"`
-	Name    string    `json:"name"`
-	Created time.Time `json:"created"`
-	Updated time.Time `json:"updated"`
+	ID          string     `json:"id"`
+	Name        string     `json:"name"`
+	Created     time.Time  `json:"created"`
+	Updated     time.Time  `json:"updated"`
+	ComponentID string     `json:"componentId"`
+	Type        ChangeType `json:"type"`
+	Content     types.JSON `json:"content"`
 }
 
 type ChangeCreate struct {
 	ComponentID string     `json:"componentId" validate:"required,min=1,max=25"`
 	Name        string     `json:"name" validate:"required,min=1,max=32"`
 	Value       string     `json:"value" validate:"required,min=1,max=255"`
-	Type        string     `json:"type" validate:"required,min=1,max=32"`
+	Type        ChangeType `json:"type" validate:"required"`
 	Content     types.JSON `json:"content" validate:"required,min=1,max=99999"`
 }
